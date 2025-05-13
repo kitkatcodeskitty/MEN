e6cf2e7fdba0834cfe24944453ebf8c9854be10e
Dilan <dilandhakal889@gmail.com>
2025-05-13 11:23:55 +0545

    push code of mernm

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.get()
+app.get("/", (req, res) =>{
+    res.status(200).send("welcome to first mern page")
+})
+
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log(`server is running in : ${PORT}`)
+})
+
+
+app.get("/register",(req,res)=>{
+    res.status(200).send("this is register")
+})
+
+app.get("/home",(req,res)=>{
+    res.status(200).send("this is home");
+})
