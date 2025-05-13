fc656c1b0bd6dd790742fc6cf200181bd93c59c0
Dilan <dilandhakal889@gmail.com>
2025-05-13 11:38:55 +0545

    push code of mernm

--- Server/router/auth-router.js
+++ Server/router/auth-router.js
@@ -1,11 +1,16 @@
 const express  = require("express");
 const router = express.Router();
 
-router.get("/register",(req,res)=>{
+router.get("/",(req,res)=>{
     res.
     status(200)
-    .send("this is register from router ");
+    .send("this is home  from router ");
 })
+
+router.get("/register",((req,res)=>{
+    res.status(200).send("this is register page");
+}))
+
 
 
 module.exports = router;
