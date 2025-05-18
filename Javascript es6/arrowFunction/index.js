fd6c9b95fba4ff5e2c3b9d783b32c2e6865304f7
Dilan <dilandhakal889@gmail.com>
2025-05-18 17:37:34 +0545

    add code of arrow function

---
+++ Javascript es6/arrowFunction/index.js
@@ -0,0 +1,114 @@
+
+// question no 1
+const add = (a,b) =>{
+    return a + b;
+}
+
+
+console.log(add(6,6));
+
+
+// question no 2 
+const square = (square) => {
+    return square*square;
+}
+
+console.log(square(5));
+
+// question no 3
+const greet = (name) =>{
+    return `Hello, ${name}`;
+}
+
+console.log(greet());
+
+// question no 4
+const es = ()=>{
+    return `I am learning ES6`;
+}
+console.log(es());
+
+
+
+// intermediate 
+const numbers = [1,2,3,4]
+
+const arrayOfNumbers = numbers.map((x)=>{
+    return x * 2;
+})
+
+console.log(arrayOfNumbers);
+
+// im doing this again so that i can remember it cuz .map is new to me 
+const num = [2,3,4];
+const squareOfNum = num.map((y)=>{
+    return y * y;
+})
+
+console.log(squareOfNum);
+
+
+// iseven
+const isEven = (n)=>{
+    if(n%2==0)
+    {
+        console.log(`${n} is even`);
+    }else{
+        console.log("Is odd");
+    }
+}
+
+console.log(isEven(6))
+
+
+// filter
+const arr = [5,12,8,130,33]
+
+const filterArr = arr.filter((x)=>{
+    return x>10;
+})
+
+console.log(filterArr);
+
+// using filter again so that i can get used to it 
+const word = ["Amelia","Kokomi","Amy","Varesa"];
+
+const wordArr = word.filter((x)=>{
+    return x.length > 5;
+})
+
+console.log(wordArr);
+
+// arrowfunction that return another arrow function to multiply a number 
+
+const double = (x) =>{
+    const mul = (x)=>{
+        return x * x; 
+    }
+}
+console.log(double);
+// i don;t understand this so help me when u are checking the code okie 
+
+
+// next question 
+const person = {
+    name: "potato",
+    greet : ()=>{
+       return `Hi,I'm ${this.name}`
+    }
+};
+console.log(person.greet());
+// i think this will return the hi, i am potato cuz we call person.greet but but since it doesn't have console.log i believe it willnot shows on console but it will surely return the statement but i see on console and it return undefine  and i try writing return infront of statement it return string but not this.name it;'s empty there
+
+
+// question not last of  Intermediate  
+const personAgain = {
+    name: 'Potato',
+    greet: function(){
+        `Hi,I'm ${this.name}`
+    }
+};
+console.log(personAgain.greet());
+// i don't understand this one too maybe i didn't undersand the question all i undersand was i need to change arrow function to normal one 
+
+// please check everyone of it and tell me where can i improve and u can drop some question too if u think i need to do or if this is enough 
