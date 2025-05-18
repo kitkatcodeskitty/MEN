cd0d26e74f40eda8e46d0df1a5374b18b3f0dd61
Dilan <dilandhakal889@gmail.com>
2025-05-18 11:38:38 +0545

    add code

--- Server/router/auth-router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth-controller");
-
-
-router.route("/home").get(authController.home);
-router.route("/register").post(authController.register);
-
-module.exports = router;
