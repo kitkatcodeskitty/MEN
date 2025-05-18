8b3eb732ab7a9cb47057741e90ed4665dd3afef6
Dilan <dilandhakal889@gmail.com>
2025-05-18 18:38:08 +0545

    add code

--- objects/index.js
+++ objects/index.js
@@ -3,4 +3,24 @@ class ShoppingList {
         this.items = items,
         this.num = items
     }
+    saylist(){
+        console.log(this.items);
+    }
 }
+
+const myList = new ShoppingList(["Milk,choco,redbull"],3);
+
+class Product extends ShoppingList {
+    constructor(items,num, amount,cost){
+        super(items,num);
+        this.amount = amount;
+        this.cost = cost;
+    }
+}
+
+const product = new Product(['Redbull, Chocolate, candy'],3, 55,123);
+
+
+
+
+console.log(product);
